51ab26b05cdea43f95a566e1bcfedb68300e66f3
Carlo Bravo <carlo.anthony70@gmail.com>
2025-06-12 12:15:35 -0500

    dia19

---
+++ DIA-19/main.js
@@ -0,0 +1,19 @@
+/* arrays desestructuracion y propgacion ejercicios */
+let a = [1, 2, 3];
+let b = ["a","b","c"];
+
+/* SALIDA ESPERADA:[1,a,2,b,3,c] */
+
+let salida = [...a, ...b];
+console.log(salida);
+
+
+
+/* objetos */
+/* let profesional = {
+    carrera: "medico",
+    especialidad: "pediatra",
+} */
+
+/* agregar las llaves de turno y lista de paceientes 
+que sera un array a ese objeto convirtiendolo en un superProfesional */
