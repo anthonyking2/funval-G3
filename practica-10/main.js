eb8c05f9108f9c1362e6dee63e85acc2213a8fde
Carlo Bravo <carlo.anthony70@gmail.com>
2025-06-22 22:15:49 -0500

    practica10

---
+++ practica-10/main.js
@@ -0,0 +1,26 @@
+async function fetchUsers() {
+    const contenedor = document.querySelector('.users-container');
+
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await res.json();
+
+        users.forEach(user => {
+            contenedor.innerHTML += `
+            <div class=" bg-blue-400 rounded shadow p-4 w-full max-w-sm">
+              <h3 class="font-bold text-lg mb-2">Nombre: ${user.name}</h3>
+              <p><strong>Usuario:</strong> ${user.username}</p>
+              <p><strong>Email:</strong> ${user.email}</p>
+              <p><strong>Empresa:</strong> ${user.company.name}</p>
+            </div>
+          `;
+        });
+    } catch (e) {
+        contenedor.innerHTML = `
+          <p class="text-red-600 bg-red-100 p-4 rounded"> Error al cargar los usuarios. Inténtalo de nuevo más tarde.</p>
+        `;
+    }
+}
+
+fetchUsers();
+
